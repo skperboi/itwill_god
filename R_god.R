8c3590b5be92447832fdbf45ebb75fbb69a8fc03
Joe <esj1020@gmail.com>
2017-12-22 17:05:59 +0900

    12.22
    commit

---
+++ R_god.R
@@ -0,0 +1,15 @@
+
+getwd()
+setwd("C:\\Users\\STU\\Documents\\GitHub")
+
+getwd()
+setwd("C:\\Users\\STU\\Documents\\GitHub")
+
+getwd()
+setwd("C:\\Users\\STU\\Documents\\GitHub")
+
+getwd()
+setwd("C:\\Users\\STU\\Documents\\GitHub")
+
+getwd()
+setwd("C:\\Users\\STU\\Documents\\GitHub")
