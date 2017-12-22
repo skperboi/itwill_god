596dd281a47e78a1b4f03f30db62674f50b8f792
Joe <esj1020@gmail.com>
2017-12-22 17:11:12 +0900

    12.22
    commit

--- R_god.R
+++ R_god.R
@@ -1,15 +1,3 @@
 
-getwd()
-setwd("C:\\Users\\STU\\Documents\\GitHub")
 
-getwd()
-setwd("C:\\Users\\STU\\Documents\\GitHub")
-
-getwd()
-setwd("C:\\Users\\STU\\Documents\\GitHub")
-
-getwd()
-setwd("C:\\Users\\STU\\Documents\\GitHub")
-
-getwd()
-setwd("C:\\Users\\STU\\Documents\\GitHub")
+paste("깃허브연습")
